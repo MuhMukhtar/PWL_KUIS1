9de139e20049d097fd122bd2e21452aae80ab193
= <=>
2021-03-12 22:08:08 +0700

    Add Controllers for Home, Menu, Post

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,22 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Menu;
 
 class HomeController extends Controller
 {
 
     public function index()
     {
-        return view('index');
+        return view('index', ['populars' => Post::popular()]);
     }
 
     public function todaySpecial()
     {
-        return view('today-special');
+        return view('today-special', ['posts' => Post::index()]);
     }
     public function menu()
     {
-        return view('menu');
+        return view('menu', ['menus' => Menu::menu()]);
     }
     public function contact()
     {
