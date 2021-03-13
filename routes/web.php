f096334cbdd83d6d3dfda49abb0aac2b7b1126c5
= <=>
2021-03-13 22:30:51 +0700

    Fix Routes Web

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/menu', [App\Http\Controllers\HomeController::class, 'menu']);
 Route::get('/contact', [App\Http\Controllers\HomeController::class, 'contact']);
 Route::get('/barang', [App\Http\Controllers\BarangController::class, 'index']);
 Route::get('/pelanggan', [App\Http\Controllers\PelangganController::class, 'index']);
+Route::get('/pegawai', [App\Http\Controllers\PegawaiController::class, 'index']);
+Route::get('/supplier', [App\Http\Controllers\SupplierController::class, 'index']);
